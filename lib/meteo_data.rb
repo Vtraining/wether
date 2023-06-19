a7432d30e115678449852e890463457f1e5710ec
Vtraining <40.s.a.m.y.40@gmail.com>
2023-06-19 19:05:31 +0300

    last commit of this app

--- lib/meteo_data.rb
+++ lib/meteo_data.rb
@@ -1,7 +1,54 @@
 require 'date'
 
 class MeteoData
-  TIME_OF_DAY = %w(night morning day evening)
-  CLOUDINESS = %w()
+  TIME_OF_DAY = %w(night morning day evening).freeze
+  CLOUDINESS = %w(clear partly_cloudly cloudly mainly_cloudly).freeze
 
+  def initialize(params)
+    @date = params[:date]
+    @time_of_day = params[:time_of_day]
+    @temperature_min = params[:temperature_min]
+    @temperature_max = params[:temperature_max]
+    @cloudiness = params[:cloudiness]
+    @max_wind = params[:max_wind]
+  end
+
+  def self.from_xml(node)
+    day = node.attributes['day']
+    month = node.attributes['month']
+    year = node.attributes['year']
+
+    new(
+      date: Date.parse("#{day}.#{month}.#{year}"),
+      time_of_day: TIME_OF_DAY[node.attributes['tod'].to_i],
+      temperature_max: node.elements['TEMPERATURE'].attributes['max'].to_i,
+      temperature_min: node.elements['TEMPERATURE'].attributes['min'].to_i,
+      cloudiness: node.elements['PHENOMENA'].attributes['cloudiness'].to_i,
+      max_wind: node.elements['WIND'].attributes['max'].to_i
+    )
+  end
+
+  def to_s
+    result = today? ? 'Today' : @date.strftime('%d.%m.%Y')
+
+    result << ", #{@time_of_day}\n" \
+      "#{temperature_range_string}, wind #{@max_wind} м/с, " \
+      "#{CLOUDINESS[@cloudiness]}"
+
+    result
+  end
+
+  def temperature_range_string
+    result = ''
+    result << '+' if @temperature_min > 0
+    result << "#{@temperature_min}.."
+    result << '+' if @temperature_max > 0
+    result << @temperature_max.to_s
+    result
+  end
+
+  def today?
+    @date == Date.today
+  end
 end
+
--- meteoservice.rb
+++ meteoservice.rb
@@ -1,36 +1,21 @@
 require 'net/http'
 require 'uri'
 require 'rexml/document'
-
+require_relative 'lib/meteo_data'
 URL = "https://xml.meteoservice.ru/export/gismeteo/point/35.xml".freeze
 
-response = Net::HTTP.get_response(URL.parse(URL))
+response = Net::HTTP.get_response(URI.parse(URL))
 
 doc = REXML::Document.new(response.body)
 
-city_name = URI.uniscape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
+city_name = URI.decode_www_form(doc.root.elements['REPORT/TOWN'].attributes['sname'])
 
-forecast_nodes = doc.root.elements['REPORT/TOWN']
-
-current_forecast = doc.root.elements['REPORT/TOWN'].elements.to_a[0]
-puts current_forecast.attributes['day']
-#a = current_forecast.root.elements.to_a
-#puts a.attributes['day']
-min_temp = current_forecast.elements['TEMPERATURE'].attributes['min']
-max_temp = current_forecast.elements['TEMPERATURE'].attributes['max']
-
-max_wind = current_forecast.elements['WIND'].attributes['max']
-
-cloud_index = current_forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
-clouds = CLOUDINESS[cloud_index]
-
-#day = current_forecast.elements['FORECAST'].tod
+forecast_nodes = doc.root.elements['REPORT/TOWN'].elements.to_a
 
 puts city_name
-#puts day
-puts "Temperature: from #{min_temp} to #{max_temp}"
-puts "Wind is: #{max_wind} m/s"
-puts clouds
+puts
 
-
-                        
+forecast_nodes.each do |node|
+  puts MeteoData.from_xml(node)
+  puts
+end
